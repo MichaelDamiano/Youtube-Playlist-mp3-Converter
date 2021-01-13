018d3f319d8ec694bd9b8e0c14026266401311c8
Michael Damiano <48996530+MichaelDamiano@users.noreply.github.com>
2021-01-12 21:16:47 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-var playlistIDs = [];
-
+//Creating a map of the playlist videos with the keys being the titles of the videos and the values being the URLs of the videos
+var videoList = new Map();
+let jsonObject = {};
 var port = chrome.extension.connect({name: "youtube"});
-var videos = document.querySelectorAll('ytd-playlist-video-renderer');
-    
-for (var i = 0; i < videos.length; i++)
+var playlistURLS = document.querySelectorAll('a.yt-simple-endpoint.style-scope.ytd-playlist-video-renderer');
+var playlistTitles = document.querySelectorAll('span#video-title.style-scope.ytd-playlist-video-renderer');
+for (var i = 0; i < playlistURLS.length; i++)
 {
-    playlistIDs.push(videos[i].children[1].children[0].href);
+    videoList.set(playlistTitles[i].title, playlistURLS[i].href);
 }   
+//converting map into JSON to send to background.js
+videoList.forEach((value, key) => {  
+    jsonObject[key] = value;  
+});  
 
-port.postMessage(playlistIDs);
+port.postMessage(JSON.stringify(jsonObject));
