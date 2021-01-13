d9b87a90f3822165bcf4e858147939f1a7bb6c94
Michael Damiano <48996530+MichaelDamiano@users.noreply.github.com>
2021-01-12 21:30:57 -0500

    Update download.js
    added error handling

--- download.js
+++ download.js
@@ -1,19 +1,27 @@
  var port = chrome.extension.connect({name: "download"});
- var notStarted = true;
+ var interval;
  port.postMessage("url");
  
  port.onMessage.addListener(function(msg) {
    document.getElementById('input').value = msg;
    document.getElementById('submit').click();
-   setInterval(download, 1000);
+   interval = setInterval(download, 1000);
 });
 
 function download(){
-   if (document.getElementById('title').innerHTML.trim() != 'Please insert a valid video URL' && notStarted)
-   {
-       notStarted = false;
-       document.getElementById('buttons').children[0].click();
-       port.postMessage("");
-       setTimeout(function(){ close(); }, 1000);
+    try{
+        if (document.getElementById('title').innerHTML.trim() != 'Please insert a valid video URL')
+        {
+            clearInterval(interval);
+            document.getElementById('buttons').children[0].click();
+        }
+    }
+   catch(err) {
+        clearInterval(interval);
+        port.postMessage("error");
    }
+   finally{
+        port.postMessage("done");
+        setTimeout(function(){ close(); }, 1000);
+    }
 }
