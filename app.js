8f770598f5b1c8dd65318d33bca93b17b4c612d6
Michael Damiano <48996530+MichaelDamiano@users.noreply.github.com>
2021-03-16 21:43:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ var videoList = new Map();
 let jsonObject = {};
 var port = chrome.extension.connect({name: "youtube"});
 var playlistURLS = document.querySelectorAll('a.yt-simple-endpoint.style-scope.ytd-playlist-video-renderer');
-var playlistTitles = document.querySelectorAll('span#video-title.style-scope.ytd-playlist-video-renderer');
 for (var i = 0; i < playlistURLS.length; i++)
 {
-    videoList.set(playlistTitles[i].title, playlistURLS[i].href);
+    videoList.set(playlistURLS[i].title, playlistURLS[i].href);
 }   
 //converting map into JSON to send to background.js
 videoList.forEach((value, key) => {  
